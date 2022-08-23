897e7e2b561a97b33d6554b436a394e367e68e9d
cjhird <charlie.hird@googlemail.com>
2022-08-23 17:23:36 +0100

    username and email dynamic

--- src/components/users/UserProfile.js
+++ src/components/users/UserProfile.js
@@ -44,107 +44,98 @@ const UserProfile = () => {
   }, [])
 
   return (
-    <>
-      <h1>User Profile Page</h1>
-      <h2>{profile.userName} </h2>
-    </>
+    <Container className="profile-wrapper mb-5">
+      <section className="section about-section gray-bg" id="about">
+        <div className="container">
+          <div className="row align-items-center flex-row-reverse">
+            <div className="col-lg-6">
+              <div className="about-text go-to">
+                <h3 className="dark-color">Profile</h3>
+                <div className="row about-list">
+                  <div className="col">
+                    <div className="media">
+                      <label>Username</label>
+                      <p>{profile.userName}</p>
+                    </div>
+                    <div className="media">
+                      <label>E-mail</label>
+                      <p>{profile.email}</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6 mt-4">
+              <div className="about-avatar">
+                <img
+                  className="profile-avatar"
+                  src="https://i.pinimg.com/474x/ab/7f/8b/ab7f8b18534abf0842b01d8ec37f4f71--funny-shit-funny-stuff.jpg"
+                  title=""
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+          <div className="counter">
+            <div className="row">
+              <div className="col">
+                <div className="count-data text-center">
+                  <h6 className="count h2 mt-4" data-to="150" data-speed="150">
+                    Previous Comments
+                  </h6>
+                  <Accordion defaultActiveKey="0">
+                    <Accordion.Item eventKey="0">
+                      <Accordion.Header>Comment #1</Accordion.Header>
+                      <Accordion.Body>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore
+                        magna aliqua. Ut enim ad minim veniam, quis nostrud
+                        exercitation ullamco laboris nisi u
+                      </Accordion.Body>
+                    </Accordion.Item>
+                    <Accordion.Item eventKey="1">
+                      <Accordion.Header>Comment #2</Accordion.Header>
+                      <Accordion.Body>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore
+                        magna aliqua. Ut enim ad minim veniam, quis nostrud
+                        exercitation ullamco laboris nisi ut
+                      </Accordion.Body>
+                    </Accordion.Item>
+                  </Accordion>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* <div className="recommend">
+              <div className="row">
+                <div className="col">
+                  <h3>Recommended Movies</h3>
+                  <div className="movie-single-wrapper text-center m-4">
+                    <Carousel fade>
+                      <Carousel.Item>
+                        <h3> Image one </h3>
 
-    // <Container className="profile-wrapper mb-5">
-    //   <section className="section about-section gray-bg" id="about">
-    //     <div className="container">
-    //       <div className="row align-items-center flex-row-reverse">
-    //         <div className="col-lg-6">
-    //           <div className="about-text go-to">
-    //             <h3 className="dark-color">Profile</h3>
-    //             <div className="row about-list">
-    //               <div className="col">
-    //                 <div className="media">
-    //                   <label>Name</label>
-    //                   <p>Woody</p>
-    //                 </div>
-    //                 <div className="media">
-    //                   <label>Username</label>
-    //                   <p>Allen's Toy</p>
-    //                 </div>
-    //                 <div className="media">
-    //                   <label>E-mail</label>
-    //                   <p>fave@domain.com</p>
-    //                 </div>
-    //               </div>
-    //             </div>
-    //           </div>
-    //         </div>
-    //         <div className="col-lg-6 mt-4">
-    //           <div className="about-avatar">
-    //             <img
-    //               className="profile-avatar"
-    //               src="https://i.pinimg.com/474x/ab/7f/8b/ab7f8b18534abf0842b01d8ec37f4f71--funny-shit-funny-stuff.jpg"
-    //               title=""
-    //               alt=""
-    //             />
-    //           </div>
-    //         </div>
-    //       </div>
-    //       {/* <div className="counter">
-    //         <div className="row">
-    //           <div className="col">
-    //             <div className="count-data text-center">
-    //               <h6 className="count h2 mt-4" data-to="150" data-speed="150">
-    //                 Previous Comments
-    //               </h6>
-    //               <Accordion defaultActiveKey="0">
-    //                 <Accordion.Item eventKey="0">
-    //                   <Accordion.Header>Comment #1</Accordion.Header>
-    //                   <Accordion.Body>
-    //                     Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-    //                     sed do eiusmod tempor incididunt ut labore et dolore
-    //                     magna aliqua. Ut enim ad minim veniam, quis nostrud
-    //                     exercitation ullamco laboris nisi u
-    //                   </Accordion.Body>
-    //                 </Accordion.Item>
-    //                 <Accordion.Item eventKey="1">
-    //                   <Accordion.Header>Comment #2</Accordion.Header>
-    //                   <Accordion.Body>
-    //                     Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-    //                     sed do eiusmod tempor incididunt ut labore et dolore
-    //                     magna aliqua. Ut enim ad minim veniam, quis nostrud
-    //                     exercitation ullamco laboris nisi ut
-    //                   </Accordion.Body>
-    //                 </Accordion.Item>
-    //               </Accordion>
-    //             </div>
-    //           </div>
-    //         </div>
-    //       </div> */}
-    //       {/* <div className="recommend">
-    //           <div className="row">
-    //             <div className="col">
-    //               <h3>Recommended Movies</h3>
-    //               <div className="movie-single-wrapper text-center m-4">
-    //                 <Carousel fade>
-    //                   <Carousel.Item>
-    //                     <h3> Image one </h3>
+                        <Carousel.Caption></Carousel.Caption>
+                      </Carousel.Item>
+                      <Carousel.Item>
+                        <h3> Image two </h3>
 
-    //                     <Carousel.Caption></Carousel.Caption>
-    //                   </Carousel.Item>
-    //                   <Carousel.Item>
-    //                     <h3> Image two </h3>
+                        <Carousel.Caption></Carousel.Caption>
+                      </Carousel.Item>
+                      <Carousel.Item>
+                        <h3> Image 3 </h3>
 
-    //                     <Carousel.Caption></Carousel.Caption>
-    //                   </Carousel.Item>
-    //                   <Carousel.Item>
-    //                     <h3> Image 3 </h3>
-
-    //                     <Carousel.Caption></Carousel.Caption>
-    //                   </Carousel.Item>
-    //                 </Carousel>
-    //               </div>
-    //             </div>
-    //           </div>
-    //         </div> */}
-    //     </div>
-    //   </section>
-    // </Container>
+                        <Carousel.Caption></Carousel.Caption>
+                      </Carousel.Item>
+                    </Carousel>
+                  </div>
+                </div>
+              </div>
+            </div> */}
+        </div>
+      </section>
+    </Container>
   )
 }
 
